3d9484d47f4342bd67dbdba05688f0881b56d0cf
luizfx22 <luizgomes749@gmail.com>
2022-08-28 22:08:58 -0300

    Replaced to tsx

---
+++ pages/api/hello.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {} from "supabase";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ name: "John Doe" });
+}
